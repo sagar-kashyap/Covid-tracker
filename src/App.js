761c67665f16379b0126e191b7d892548318cd7a
Sagar Kashyap <sagarkashyap.cc@gmail.com>
2022-04-13 11:47:52 +0530

    file strutured

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import React from 'react';
+import styles from './App.module.css';
+import Cards from './components/Cards/Cards';
+import Charts from './components/Charts/Charts';
+import CountryPicker from './components/CountryPicker/CountryPicker';
+import {fetchData} from './api/api.index';
 
-import './App.css';
+class App extends React.Component {
 
-function App() {
-  return (
-    <div>
-      <h1>App</h1>
-    </div>
-  );
-}
+  state={
+    data:{},
+  }
+
+  async componentDidMount(){
+    const fetchedData=await fetchData();
+    this.setState({data:fetchedData});
+  }
+
+  render(){
+    const {data}=this.state;
+    return (
+      <div className={styles.container}>
+        <Cards data={data}/>
+        <Charts />
+        <CountryPicker />
+  
+      </div>
+    );
+  }
+  }
+  
 
 export default App;
---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Card,CardContent, Typography, Grid} from '@material-ui/core'
+import { StylesContext } from '@material-ui/styles';
+import styles from './Card.module.css'
+
+const Cards=(props)=>{
+    console.log(props)
+    return(
+        <div className={styles.container}>
+
+        </div>
+        
+    )
+}
+
+export default Cards;
