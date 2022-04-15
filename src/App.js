df704eab5adec856c0aa3eb25e9239c552e4e3b4
Sagar Kashyap <sagarkashyap.cc@gmail.com>
2022-04-15 14:43:54 +0530

    line chart added

--- src/App.js
+++ src/App.js
@@ -14,13 +14,14 @@ class App extends React.Component {
   async componentDidMount(){
     const fetchedData=await fetchData();
     this.setState({data:fetchedData});
+    
   }
 
   render(){
-    const {data}=this.state;
+    const { data } =this.state;
     return (
       <div className={styles.container}>
-        <Cards data={data}/>
+        <Cards data={data} />
         <Charts />
         <CountryPicker />
   
--- src/components/Charts/Charts.jsx
+++ src/components/Charts/Charts.jsx
@@ -1,8 +1,43 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import {fetchDailyData} from '../../api/api.index';
+import {Line,Bar} from 'react-chartjs-2';
+import {Chart as ChartJS} from 'chart.js/auto';
+import styles from './Charts.module.css';
 
 const Charts=()=>{
+    const [dailyData,setDailyData]=useState([]);
+
+    useEffect(()=>{
+        const fetchAPI=async()=>{
+            setDailyData(await fetchDailyData());
+        }
+        
+        fetchAPI();
+    });
+    const lineChart=(
+        dailyData.length!==0
+        ?
+        <Line 
+        data={{labels:dailyData.map(({date})=>date),
+        datasets:[{
+            data:dailyData.map(({confirmed})=>confirmed),
+            label:'Infected',
+            borderColor:'#3333ff',
+            fill:true,
+        },{
+            data:dailyData.map(({deaths})=>deaths),
+            label:'Deaths',
+            borderColor:'rgba(255,0,0,0.5)',
+            fill:true,
+        }],}}>
+
+        </Line>:null
+    );
+
 return(
-        <h1>Charts</h1>
+        <div className={styles.container}>
+            {lineChart}
+        </div>   
         
     )
 }
