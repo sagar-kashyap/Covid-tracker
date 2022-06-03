fee906b7f50ca28ca3c3b9a52b8cf1a54536b391
Sagar Kashyap <54504602+sagar-kashyap@users.noreply.github.com>
2022-06-03 12:28:10 +0530

    Update api.index.js

--- src/api/api.index.js
+++ src/api/api.index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url='https://covid19.mathdro.id/api';
+const url='https://covid19.mathdro.id/api'; //covid data api(recovery data in not being updated)
 
 export const fetchData= async ()=>{
     try {
